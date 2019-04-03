58e8be377a79c2bb98686a0e044a8d0e666b1322
jlcampbell16 <45213854+Jlcampbell16@users.noreply.github.com>
2019-04-02 18:54:02 -0600

    working functions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -41,69 +41,72 @@ function displayProducts() {
 function run() {
   // connection.query("SELECT * FROM products", function (err, res) {
   //   if (err) throw err;
-    inquirer.prompt([
-      {
-        type: "input",
-        message: "Enter the ID of the item you would like to buy:",
-        name: "id",
-        validate: function (value) {
-          if (isNaN(value) === false) {
-            return true
-          }
-          console.log(`Please enter an ID number.`);
-          return false
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "Enter the ID of the item you would like to buy:",
+      name: "id",
+      validate: function (value) {
+        if (isNaN(value) === false) {
+          return true
         }
-      },
-      {
-        type: "input",
-        message: `How many units would you like to buy?`,
-        name: "units",
-        validate: function (value) {
-          if (isNaN(value) === false) {
-            return true
-          }
-          console.log(`Please enter a number.`);
-          return false
-        }
+        console.log(`Please enter an ID number.`);
+        return false
       }
-    ]).then(function (res) {
-
-      connection.query("SELECT * FROM products WHERE item_id=?", [res.id], function (err, res) {
-        if (err) throw err;
-        console.log(res[0].product_name);
-
-        // check stock & calculate total cost
-        var currentQuantity = res[0].stock_quantity;
-        console.log("current Quant:" + currentQuantity);
-        console.log("res.units:" + res.units); //UNDEFINED
-      
-        if (res.units <= currentQuantity) {
-          console.log("HELLLOOO")
-          var newQuantity = currentQuantity - res.units;
-          console.log("new Quant:" + newQuantity) //NAN 
-
-          connection.query("UPDATE products SET ? WHERE ?",
-            [
-              {
-                stock_quantity: newQuantity
-              },
-              {
-                item_id: res.id
-              }
-            ], function (err, res) {
-              if (err) throw err;
-              var totalPrice = res.units * res[0].price;
-              console.log(`\nYour total cost for ${res.units} units of ${res[0].product_name} is $${totalPrice}.`);
-              displayProducts();
-            }
-          )
+    },
+    {
+      type: "input",
+      message: `How many units would you like to buy?`,
+      name: "units",
+      validate: function (value) {
+        if (isNaN(value) === false) {
+          return true
         }
-        // else {
-        //   console.log (`Insufficient Quantity.\n\n`);
-        //   displayProducts();
-        //   }
-      })
-    });
+        console.log(`Please enter a number.`);
+        return false
+      }
+    }
+  ]).then(function (res) {
+    console.log("res:", res);
+    units = parseInt(res.units);
+    console.log("units: ", units);
+    
+    connection.query("SELECT * FROM products WHERE item_id=?", [res.id], function (err, res) {
+      if (err) throw err;
+      // check stock & calculate total cost
+      var currentQuantity = res[0].stock_quantity;
+      var price = res[0].price;
+      if (units <= currentQuantity) {
+        var newQuantity = currentQuantity - units;
+
+console.log("Res:", res)
+console.log("res[0].item_id", res[0].item_id)
+        connection.query("UPDATE products SET ? WHERE ?",
+          [
+            {
+              stock_quantity: newQuantity
+            },
+            {
+              item_id: res[0].item_id
+            }
+          ], function (err, res) {
+            if (err) throw err;
+            console.log("if callback", res)
+            var totalPrice = units * price;
+
+            console.log(`\nYour total cost is $${totalPrice}.`);
+            productsAvail = [];
+            displayProducts();
+          }
+        )
+      }
+      else {
+        console.log (`Insufficient Quantity.\n\n`);
+        productsAvail = [];
+        displayProducts();
+        }
+    })
+  });
   // });
 };
 
